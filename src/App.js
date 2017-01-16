29d10af58642ce230247e737ed82f679178dc149
Craig <craigjwaldron@gmail.com>
2017-01-16 17:12:11 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
 import React from 'react';
 
-const App = () => <h1>Hello Earth</h1>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'state text'
+    }
+  }
+  update( e ){
+    this.setState({txt: e.target.value})
+  }
+  render(){
+    return (
+      <div>
+        <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)}/>
+        <Widget update={this.update.bind(this)}/>
+        <Widget update={this.update.bind(this)}/>
+      </div>
+    )
+  }
+}
+
+const Widget = (props) =>
+  <input type="text" onChange={props.update}/>
 
 export default App
+
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// }
+//
+// App.defaultProps = {
+//   txt: "default text"
+// }
+
+// class App extends React.Component {
+//   render(){
+//     return (
+//       <div>
+//         <h1>{this.props.txt}</h1>
+//         <b>Earth</b>
+//       </div>
+//     )
+//   }
+// }
